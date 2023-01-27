20de1dd94c7eb66bc0fcdfe615bbd06f3b087352
Divyanshu <71317784+divyanshucs052@users.noreply.github.com>
2023-01-27 20:40:33 +0530

    Delete get-any-token-price directory

--- get-any-token-price/components/header.js
+++
@@ -1,9 +0,0 @@
-import styles from "../styles/Home.module.css";
-
-export default function Header() {
-  return (
-    <section>
-      <h1 className={styles.title}>What is the token price?</h1>
-    </section>
-  );
-}
